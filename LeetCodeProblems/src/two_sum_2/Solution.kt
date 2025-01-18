d199a0503b90dc9d010af0845b94b2c21d740460
Juanma Carmona <juanmacarmona97@gmail.com>
2025-01-18 02:13:35 +0100

    two_sumsII - 2 ms

---
+++ LeetCodeProblems/src/two_sum_2/Solution.kt
@@ -0,0 +1,49 @@
+package two_sum_2
+/*Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].*/
+fun main() {
+    var numbers:IntArray = intArrayOf(0,0,3,4)
+    var target:Int = 0
+
+    twoSum(numbers,target)
+}
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var left:Int = 0
+    var right:Int = numbers.size - 1
+    var sum:Int = 0
+
+    do {
+        sum = numbers[left] + numbers[right]
+        if (sum>target){
+            right--
+        }else if (sum<target){
+            left++
+        }
+    }while (sum != target)
+
+    return intArrayOf((left+1),(right+1))
+
+}
